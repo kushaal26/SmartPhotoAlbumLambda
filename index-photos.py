037968502d7cb0a9a57a9313757083a31c53ee0a
Kushaal Gajavelly <31340167+kushaal26@users.noreply.github.com>
2023-04-01 15:03:02 -0400

    uploading lambda functions

---
+++ index-photos.py
@@ -0,0 +1,54 @@
+import json
+import boto3
+import time
+import requests
+
+ES_HOST = 'https://search-photos-r7vcovxedl22yzbs5f3gv3snba.us-east-1.es.amazonaws.com'
+REGION = 'us-east-1'
+
+def get_url(index, type):
+    url = ES_HOST + '/' + index + '/' + type
+    return url
+
+def lambda_handler(event, context):
+    print("event: ", event)
+    jsonBody = event['Records'][0]
+    bucketName = jsonBody['s3']['bucket']['name']
+    key = jsonBody['s3']['object']['key']
+    reko = boto3.client('rekognition')
+    s3 = boto3.client('s3')
+    print("key" , key)
+    try:
+        data = {}
+        responses3 = s3.head_object(Bucket = bucketName, Key =key )
+        print("responses3: ", responses3)
+        
+        response = reko.detect_labels(
+            Image={'S3Object': {'Bucket': bucketName, 'Name': key}})
+        print("response: ", response)
+        data['objectKey'] = key
+        data['bucket'] = bucketName
+        data['createdTimestamp'] = time.time()
+        data['labels'] = []
+        print(responses3)
+        if not responses3['Metadata'] =={}:
+            customlabel = responses3['Metadata']['customlabels']
+            if customlabel != "":
+                data['labels'] = [i.strip() for i in customlabel.split(",")]
+        
+        for label in response['Labels']:
+            if label['Confidence'] > 95:
+                data['labels'].append(label['Name'])
+        print("***********",data['labels'])
+        body = json.dumps(data)
+        headers = {"Content-Type": "application/json"}
+        esUrl = get_url('photos', 'Photo')
+        r = requests.post(url=esUrl,auth = ('photos-user', 'Kushaal@26'), data=body, headers=headers)
+        print(body,"******",r)
+    except Exception as e:
+        print("Error " + str(e))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
---
+++ search-photos.py
@@ -0,0 +1,81 @@
+import json
+import boto3
+import os
+import sys
+import uuid
+import time
+from botocore.vendored import requests
+
+ES_HOST = 'https://search-photos-r7vcovxedl22yzbs5f3gv3snba.us-east-1.es.amazonaws.com'
+REGION = 'us-east-1'
+
+def get_url(es_index, es_type, keyword):
+    url = ES_HOST + '/' + es_index + '/' + es_type + '/_search?q=' + keyword.lower()
+    return url
+
+def lambda_handler(event, context):
+
+	print("EVENT --- {}".format(json.dumps(event)))
+	
+	headers = { "Content-Type": "application/json" }
+	lex = boto3.client('lex-runtime')
+    
+	query = event["queryStringParameters"]["q"]
+	
+	print("query: ", query)
+	lex_response = lex.post_text(
+		botName='PhotoAlbum',
+		botAlias='photoalbum',
+		userId='kelly',
+		inputText=query
+	)
+	
+	print("LEX RESPONSE --- {}".format(json.dumps(lex_response)))
+
+	slots = lex_response['slots']
+	print("slots: ", slots)
+
+	img_list = []
+	for i, tag in slots.items():
+		if tag:
+			if tag[-1] == '.':
+				tag = tag[:len(tag)-1]
+			url = get_url('photos', 'Photo', tag)
+			print("ES URL --- {}".format(url))
+
+			es_response = requests.get(url, auth=("photos-user", "Kushaal@26")).json()
+			print("ES RESPONSE --- {}".format(json.dumps(es_response)))
+
+			es_src = es_response['hits']['hits']
+			print("ES HITS --- {}".format(json.dumps(es_src)))
+
+			for photo in es_src:
+				print(photo)
+				labels = [word.lower() for word in photo['_source']['labels']]
+				if tag in labels:
+					print(tag)
+					objectKey = photo['_source']['objectKey']
+					img_url = 'https://s3.amazonaws.com/photos-bucket-1/' + objectKey
+					img_list.append(img_url)
+	
+	img_list = list(set(img_list))
+	print(img_list)
+
+	if img_list:
+		return {
+			'statusCode': 200,
+			'headers': {
+				"Access-Control-Allow-Origin": "*",
+				'Content-Type': 'application/json'
+			},
+			'body': json.dumps(img_list)
+		}
+	else:
+		return {
+			'statusCode': 200,
+			'headers': {
+				"Access-Control-Allow-Origin": "*",
+				'Content-Type': 'application/json'
+			},
+			'body': json.dumps("No such photos.")
+		}
